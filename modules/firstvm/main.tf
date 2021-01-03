868ebf2741af4af48977a3b75e4d769b541ecf89
Anish96 <sonu.anish@gmail.com>
2021-01-03 13:06:52 +0530

    second VM

--- modules/firstvm/main.tf
+++ modules/firstvm/main.tf
@@ -11,3 +11,17 @@ resource "google_compute_instance" "vminstance" {
    network = "default"
 }
 }
+
+resource "google_compute_instance" "vminstance2" {
+  name = "first-application-server"
+  machine_type = "f1-micro"
+
+  boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+}
+ network_interface {
+   network = "default"
+ }
+}
