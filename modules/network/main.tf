6eea1fb9a717a06863337c64515dccb61e368dbf
Anish96 <sonu.anish@gmail.com>
2021-01-06 15:30:11 +0530

    test network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -15,14 +15,14 @@ subnets = {
             description            = "route through IGW to access internet"
             destination_range      = "0.0.0.0/0"
             tags                   = "egress-inet"
-            next_hop_internet      = "true"
+           # next_hop_internet      = "true"
         }
         {
             name                   = "app-proxy"
             description            = "route through proxy to reach app"
             destination_range      = "10.50.10.0/24"
             tags                   = "app-proxy"
-            next_hop_instance      = "app-proxy-instance"
-            next_hop_instance_zone = "us-west1-a"
+           # next_hop_instance      = "app-proxy-instance"
+            #next_hop_instance_zone = "us-west1-a"
         }
             }
