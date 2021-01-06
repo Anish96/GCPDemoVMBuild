c99930e81f8d859f2a687ee397a700a2d5d58670
Anish96 <sonu.anish@gmail.com>
2021-01-06 15:26:12 +0530

    test network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,23 +1,22 @@
 resource "google_compute_network" "vpc_network" {
   name = "Test-vpc-network"
   }
-subnets = [
+subnets = {
             subnet_name           = "subnet-02"
             subnet_ip             = "10.10.20.0/24"
             subnet_region         = "us-west1"
             subnet_private_access = "true"
             subnet_flow_logs      = "true"
             description           = "This subnet has a description"   
-
-        ],
-            routes = [
+}
+            routes = {
         {
             name                   = "egress-internet"
             description            = "route through IGW to access internet"
             destination_range      = "0.0.0.0/0"
             tags                   = "egress-inet"
             next_hop_internet      = "true"
-        },
+        }
         {
             name                   = "app-proxy"
             description            = "route through proxy to reach app"
@@ -25,6 +24,5 @@ subnets = [
             tags                   = "app-proxy"
             next_hop_instance      = "app-proxy-instance"
             next_hop_instance_zone = "us-west1-a"
-        },
-    ]
-}
+        }
+            }
