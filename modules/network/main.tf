e9e7464698124bdc247ee1c563528cb5239b5044
Anish96 <sonu.anish@gmail.com>
2021-01-07 11:53:02 +0530

    test network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -89,5 +89,15 @@ resource "google_compute_attached_disk" "default" {
   instance = google_compute_instance.vminstance2.id
 }
 
+# -------create Compute engine using image  -----
+
+resource "google_compute_instance_from_machine_image" "vminstance3" {
+  provider = google-beta
+  name     =  Third-application-server
+  zone     = "us-central1-a"
+
+  source_machine_image = "My First Project/ultra-light-298615/global/machineImages/test-vm"
+
+}
         
            
