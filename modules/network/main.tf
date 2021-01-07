3b273de8ba3261afe4b77b79c6d0a22766cc8f76
Anish96 <sonu.anish@gmail.com>
2021-01-07 07:29:36 +0530

    test network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -3,8 +3,16 @@ resource "google_compute_network" "custom-test" {
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "custom-subnetwork" {
-  name          = "test-subnetwork"
+resource "google_compute_subnetwork" "custom-subnetwork" [
+  name          = "test-subnetwork1"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.custom-test.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  },
+  {name          = "test-subnetwork2"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
   network       = google_compute_network.custom-test.id
@@ -12,7 +20,8 @@ resource "google_compute_subnetwork" "custom-subnetwork" {
     range_name    = "tf-test-secondary-range-update1"
     ip_cidr_range = "192.168.10.0/24"
   }
-}
+]
+
 
 resource "google_compute_firewall" "custom-firewall" {
   name    = "test-firewall"
@@ -29,6 +38,7 @@ resource "google_compute_firewall" "custom-firewall" {
 
   source_tags = ["web"]
 }
+
 
 
         
