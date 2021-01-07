eeb15f8ad62c8986886a8613eb2b4520a2eb62b0
Anish96 <sonu.anish@gmail.com>
2021-01-07 06:31:18 +0530

    test network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,20 +1,18 @@
 resource "google_compute_network" "vpc_network" {
   name = "Test-vpc-network"
   }
-subnets = {
-            subnet_name           = "subnet-02"
-            subnet_ip             = "10.10.20.0/24"
-            subnet_region         = "us-west1"
-            subnet_private_access = "true"
-            subnet_flow_logs      = "true"
-            description           = "This subnet has a description"   
+  
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.custom-test.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
 }
-            route ={
-            name                 = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-           # next_hop_internet      = "true"
-        }
+
+
         
            
