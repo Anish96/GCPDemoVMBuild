ad288f587e9fe902c9f30227e617797e13b8df45
Anish96 <sonu.anish@gmail.com>
2021-01-07 10:42:45 +0530

    test network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -56,5 +56,16 @@ resource "google_compute_instance" "vminstance2" {
  }
 }
 
+resource "google_compute_disk" "default" {
+  name  = "test-disk"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  image = "debian-9-stretch-v20200805"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
+}
+
         
            
