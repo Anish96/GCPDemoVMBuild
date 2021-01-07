ced8256c9f83d4f83edb80b5e2568839b9b35a4c
Anish96 <sonu.anish@gmail.com>
2021-01-07 06:23:18 +0530

    test netowrk

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -9,20 +9,12 @@ subnets = {
             subnet_flow_logs      = "true"
             description           = "This subnet has a description"   
 }
-            route = {
-        {
+            route ={
             name                 = "egress-internet"
             description            = "route through IGW to access internet"
             destination_range      = "0.0.0.0/0"
             tags                   = "egress-inet"
            # next_hop_internet      = "true"
         }
-        {
-            name                   = "app-proxy"
-            description            = "route through proxy to reach app"
-            destination_range      = "10.50.10.0/24"
-            tags                   = "app-proxy"
-           # next_hop_instance      = "app-proxy-instance"
-            #next_hop_instance_zone = "us-west1-a"
-        }
-            }
+        
+           
