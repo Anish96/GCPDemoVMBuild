6a57138cbd071985e3bb145df2b62572888c5db3
Anish96 <sonu.anish@gmail.com>
2021-01-07 12:12:57 +0530

    test network

--- provider.tf
+++ provider.tf
@@ -6,7 +6,6 @@ provider "google" {
   zone    = "us-central1-a"
 }
 provider "google-beta" {
-  version = "3.5.0"
   credentials = var.credentials
   project = "ultra-light-298615"
   region  = "us-central1"
