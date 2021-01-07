980e014766730718e5fe31f3b4e6e8490a477f83
Anish96 <sonu.anish@gmail.com>
2021-01-07 07:32:50 +0530

    test network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -4,6 +4,7 @@ resource "google_compute_network" "custom-test" {
 }
 
 resource "google_compute_subnetwork" "custom-subnetwork" [
+    {
   name          = "test-subnetwork1"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
