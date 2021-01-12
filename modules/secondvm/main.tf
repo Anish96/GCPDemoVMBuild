85219ac9e2cdc7963fc066fa1569eeafab51ee1f
Anish96 <sonu.anish@gmail.com>
2021-01-12 17:22:29 +0530

    test network

--- modules/secondvm/main.tf
+++ modules/secondvm/main.tf
@@ -10,10 +10,12 @@ name ="server${count.index}"
      image = "debian-cloud/debian-9"
    }
 }
- #network_interface {
+ network_interface {
   # network = google_compute_network.custom-test.name
+  network = test-network
   # subnetwork = google_compute_subnetwork.custom-subnetwork2.name
- #}
+  subnetwork = test-subnetwork2
+ }
 
  labels = {
     environment = "test_tier"
