9683e1d66300099dcd9cd1e4df299456380d93ec
Anish96 <sonu.anish@gmail.com>
2021-01-12 15:13:42 +0530

    test network

--- modules/secondvm/main.tf
+++ modules/secondvm/main.tf
@@ -13,9 +13,9 @@ name ="gcp8286saq${count.index}"
    }
 }
  #network_interface {
-   network = google_compute_network.custom-test.name
-   subnetwork = google_compute_subnetwork.custom-subnetwork2.name
- }
+  # network = google_compute_network.custom-test.name
+  # subnetwork = google_compute_subnetwork.custom-subnetwork2.name
+ #}
 
  labels = {
     environment = "test_tier"
